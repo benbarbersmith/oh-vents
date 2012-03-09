77056e56763969a61b8f79049cc4319dcc5471a8
Ben Smith <ben@benjaminasmith.com>
2012-03-09 08:18:24 +0000

    Added static pages with tests and titles.

---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe PagesController do
+  render_views
+
+  describe "GET 'home'" do
+    it "should be successful" do
+      get 'home'
+      response.should be_success
+    end
+
+    it "should have correct title" do
+      get 'home'
+      response.should have_selector("title", :content => "| Home")
+    end
+  end
+
+  describe "GET 'contact'" do
+    it "should be successful" do
+      get 'contact'
+      response.should be_success
+    end
+
+    it "should have correct title" do
+      get 'contact'
+      response.should have_selector("title", :content => "| Contact")
+    end
+  end
+
+end
