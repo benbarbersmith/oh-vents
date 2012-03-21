2311be9f609472419c592469f79cf5f14c14ce83
Ben Smith <ben@benjaminasmith.com>
2012-03-21 21:31:01 +0000

    Fixed embarrassing month/minute mixup in my friendly date format.

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,5 +1,5 @@
 # config/initializers/time_formats.rb)
 
-Time::DATE_FORMATS[:friendly] = "%A %B %d, %Y at %H:%m"
+Time::DATE_FORMATS[:friendly] = "%A %B %d, %Y at %H:%M"
 Time::DATE_FORMATS[:month_and_year] = "%B %Y"
 Time::DATE_FORMATS[:short_ordinal] = lambda { |time| time.strftime("%B #{time.day.ordinalize}") }
